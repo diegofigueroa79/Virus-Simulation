010040a1ee393d9bd858d9c19b4584df408f6931
diegofigueroa79 <diegofigueroa79@gmail.com>
2018-09-09 19:07:12 -0700

    Added virus, and camera movement
    added virus, materials, and movement script

---
+++ Virus Simulation/Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour {
+
+    private GameObject target;
+    public Vector3 cameraDistance = new Vector3(0f, 1f, -5f);
+    public float distanceDamp = 4f;
+    public float rotationDamp = 8f;
+    private Transform myT;
+
+	// Use this for initialization
+	void Start () {
+        myT = transform;
+        target = GameObject.Find("virus2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        Vector3 toPosition = target.transform.position + (target.transform.rotation * cameraDistance);
+        Vector3 currentPosition = Vector3.Lerp(myT.position, toPosition, distanceDamp * Time.deltaTime);
+        myT.position = currentPosition;
+
+        Quaternion toRotation = Quaternion.LookRotation(target.transform.position - myT.position,
+            target.transform.up);
+        Quaternion currentRotation = Quaternion.Slerp(myT.rotation, toRotation, rotationDamp * Time.deltaTime);
+        myT.rotation = currentRotation;
+    }
+
+}
---
+++ Virus Simulation/Assets/Scripts/Virus_Movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus_Movement : MonoBehaviour
+{
+
+    public float moveVelocity = 7f, maxVelocity = 5f;
+    public float propulsion = 400f, maxPropulsion = 4f;
+    private float xMovement, yMovement;
+    private Rigidbody rigidb;
+    private bool swim;
+    private Animator animator;
+    Quaternion resetPosition = Quaternion.Euler(0, 0, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidb = gameObject.GetComponent<Rigidbody>();
+        animator = gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yMovement = Input.GetAxis("Vertical") * 70;
+
+        Quaternion targetRotation = Quaternion.Euler(yMovement, transform.localEulerAngles.y,
+            transform.localEulerAngles.z);
+
+        if(xMovement == 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.6f * Time.deltaTime);
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+		swim = Input.GetButton("Fire1");
+        if (rigidb.velocity.magnitude < maxVelocity)
+        {
+			rigidb.AddTorque(Vector3.up * xMovement * moveVelocity);
+        }
+
+        if (swim)
+        {
+            animator.SetTrigger("Swim");
+            if (rigidb.velocity.magnitude < maxPropulsion)
+            {
+				rigidb.AddForce(transform.forward * propulsion * Time.deltaTime);
+            }
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, resetPosition, 100 * Time.deltaTime);
+    }
+}
